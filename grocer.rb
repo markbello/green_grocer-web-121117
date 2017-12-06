f2fe62f4a5e7c1808db4cba8653fb2027a462d73
Mark Bello <mcbello51286@gmail.com>
2017-12-06 21:08:11 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,39 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  cart.each{|item|
+    item.each{|key, value|
+      consolidated_cart[key] = value
+      !consolidated_cart[key][:count] ? consolidated_cart[key][:count] = 1 : consolidated_cart[key][:count] +=1
+    }
+  }
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons_to_apply = cart.map {|cart_item, attributes|
+    applied_coupon = coupons.map{
+      |coupon_item|
+      # binding.pry
+      if coupon_item[:item] == cart_item && (attributes[:count] % coupon_item[:num] == 0)
+        attributes[:count] -= coupon_item[:num]
+        binding.pry
+        coupon_title = "#{cart_item} W/COUPON"
+        coupon_hash = attributes
+        # binding.pry
+        coupon_hash[:count] = 1
+        coupon_hash[:price] = coupon_item[:cost]
+        # binding.pry
+      end
+      coupon_return_hash = {}
+      coupon_return_hash[coupon_title] = coupon_hash
+      coupon_return_hash
+    }
+    applied_coupon
+  }
+  coupons_to_apply.flatten!
+  cart.merge(coupons_to_apply[0])
 end
 
 def apply_clearance(cart)
